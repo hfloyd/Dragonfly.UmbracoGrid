c206116282822672d4494ec6e2ad36b78b210b97
Heather Floyd <heather@floydinnovations.com>
2017-10-03 12:51:42 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Dragonfly.UmbracoGrid #
+
+A collection of Umbraco Grid helpers created by [Heather Floyd](https://www.HeatherFloyd.com).
---
+++ src/Dragonfly/GridHelper.cs
@@ -0,0 +1,69 @@
+﻿namespace Dragonfly
+{
+    using System.Linq;
+    using Skybrud.Umbraco.GridData;
+
+    class GridHelper
+    {
+        public static bool GridHasContent(GridDataModel GridControlData)
+        {
+            var hasContent = false;
+            var keepLooking = true;
+
+            while (keepLooking)
+            {
+                if (GridControlData.Sections.Any())
+                {
+                    foreach (var section in GridControlData.Sections)
+                    {
+                        if (section.Rows.Any())
+                        {
+                            foreach (var row in section.Rows)
+                            {
+                                if (row.Areas.Any())
+                                {
+                                    foreach (var col in row.Areas)
+                                    {
+                                        if (col.Controls.Any())
+                                        {
+                                            hasContent = true;
+                                            keepLooking = false;
+                                            break;
+                                        }
+                                    }
+                                }
+                                else
+                                { keepLooking = false; }
+                            }
+                        }
+                        else
+                        { keepLooking = false; }
+                    }
+                }
+                else
+                { keepLooking = false; }
+            }
+
+
+            //var allControls = new List<GridControl>();
+            //foreach (var section in GridControlData.Sections)
+            //{
+            //    foreach (var row in section.Rows)
+            //    {
+            //        foreach (var col in row.Areas)
+            //        {
+            //            foreach (var control in col.Controls)
+            //            {
+            //                allControls.Add(control);
+            //            }
+            //        }
+            //    }
+            //}
+
+            //if (allControls.Any())
+            //{ hasContent = true; }
+
+            return hasContent;
+        }
+    }
+}
